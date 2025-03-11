af56f94d6e7ca4c0de6772ee1bfc377ffd7dffac
Jhonatan <play4fungameplay@gmail.com>
2025-03-11 15:42:21 -0300

    Criada a rota de login

---
+++ src/controllers/login.go
@@ -0,0 +1,48 @@
+package controllers
+
+import (
+	"api/src/banco"
+	"api/src/models"
+	"api/src/repository"
+	"api/src/respostas"
+	"api/src/seguranca"
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+// Login é responsável por autenticar o usuário na API
+func Login(w http.ResponseWriter, r *http.Request) {
+	corpoDaRequisicao, erro := io.ReadAll(r.Body)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnprocessableEntity, erro)
+		return
+	}
+
+	var usuario models.Usuario
+	if erro = json.Unmarshal(corpoDaRequisicao, &usuario); erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
+
+	repositorio := repository.NovoRepositorioDeUsuarios(db)
+	usuarioSalvoNoBanco, erro := repositorio.BuscarPorEmail(usuario.Email)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	if erro = seguranca.VerificarSenha(usuarioSalvoNoBanco.Senha, usuario.Senha); erro != nil {
+		respostas.Erro(w, http.StatusUnauthorized, erro)
+		return
+	}
+
+	w.Write([]byte("Você está logado! Parabéns!"))
+}

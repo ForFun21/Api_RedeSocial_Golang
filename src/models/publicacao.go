9a9407376b30b860de5dfa3883b871fc21f7f48e
Jhonatan <play4fungameplay@gmail.com>
2025-04-28 14:43:11 -0300

    criado os metodos para validacao da publicacao

--- src/models/publicacao.go
+++ src/models/publicacao.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 // Publicacao representa uma publicação feita por um usuário
 type Publicacao struct {
@@ -11,4 +15,30 @@ type Publicacao struct {
 	AutorNick uint64    `json:"autorNick,omitempty"`
 	Curtidas  uint64    `json:"curtidas"`
 	CriadaEm  time.Time `json:"criadaEm,omitempty"`
+}
+
+// Preparar vai chamar os métodos para validar e formatar a publicação recebida
+func (publicacao *Publicacao) Preparar() error {
+	if erro := publicacao.Validar(); erro != nil {
+		return erro
+	}
+
+	publicacao.formatar()
+	return nil
+}
+
+func (publicacao *Publicacao) Validar() error {
+	if publicacao.Titulo == "" {
+		return errors.New("o título da publicação não pode estar em branco")
+	}
+	if publicacao.Conteudo == "" {
+		return errors.New("o conteúdo da publicação não pode estar em branco")
+	}
+
+	return nil
+}
+
+func (publicacao *Publicacao) formatar() {
+	publicacao.Titulo = strings.TrimSpace(publicacao.Titulo)
+	publicacao.Conteudo = strings.TrimSpace(publicacao.Conteudo)
 }

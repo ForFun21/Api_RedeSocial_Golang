88b8a7de8a78151d745323f07d88381a439b5493
Jhonatan <play4fungameplay@gmail.com>
2025-04-28 13:49:37 -0300

    Criada as rotas de publicacoes

---
+++ src/router/rotas/publicacoes.go
@@ -0,0 +1,39 @@
+package rotas
+
+import (
+	"api/src/controllers"
+	"net/http"
+)
+
+var rotasPublicacoes = []Rota{
+	{
+		URI:                "/publicacoes",
+		Metodo:             http.MethodPost,
+		Funcao:             controllers.CriarPublicacao,
+		RequerAltenticacao: true,
+	},
+	{
+		URI:                "/publicacoes",
+		Metodo:             http.MethodGet,
+		Funcao:             controllers.BuscarPublicacoes,
+		RequerAltenticacao: true,
+	},
+	{
+		URI:                "/publicacoes/{publicacaoId}",
+		Metodo:             http.MethodGet,
+		Funcao:             controllers.BuscarPublicacao,
+		RequerAltenticacao: true,
+	},
+	{
+		URI:                "/publicacoes/{publicacaoId}",
+		Metodo:             http.MethodPut,
+		Funcao:             controllers.AtualizarPublicacao,
+		RequerAltenticacao: true,
+	},
+	{
+		URI:                "/publicacoes/{publicacaoId}",
+		Metodo:             http.MethodDelete,
+		Funcao:             controllers.DeletarPublicacao,
+		RequerAltenticacao: true,
+	},
+}

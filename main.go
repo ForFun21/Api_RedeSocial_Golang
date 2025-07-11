6b3497fabba4c4f43d4e28dee30235114b3c1353
Jhonatan <play4fungameplay@gmail.com>
2025-07-10 22:03:12 -0300

    adicionado cors ao projeto

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/handlers"
 	_ "github.com/lib/pq"
 )
 
@@ -14,6 +15,17 @@ func main() {
 	config.Carregar()
 	r := router.Gerar()
 
-	fmt.Printf("Escutando na porta %d", config.Porta)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Porta), r))
+	cors := handlers.CORS(
+		handlers.AllowedOrigins([]string{"http://localhost:3000"}), // seu front local
+		handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}),
+		handlers.AllowedHeaders([]string{"Content-Type", "Authorization"}),
+	)
+
+	fmt.Printf("Escutando na porta %d\n", config.Porta)
+	log.Fatal(
+		http.ListenAndServe(
+			fmt.Sprintf(":%d", config.Porta),
+			cors(r), // aqui envolvemos o router no CORS
+		),
+	)
 }
